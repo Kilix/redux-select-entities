2c735270339634d6877ed2c07c9d8c63019b650e
Augustin Le Fèvre <aulefevre@kilix.fr>
2017-03-02 11:05:14 +0100

    Initial entityReducer implementation

---
+++ src/__tests__/entityReducer.spec.js
@@ -0,0 +1,120 @@
+import { schema, normalize } from 'normalizr';
+
+import entityReducer from '../entityReducer';
+
+const todoSchema = new schema.Entity('todo');
+
+describe('entityReducer', () => {
+    it('should return a reducer', () => {
+        const reducer = () => {};
+        const hor = entityReducer(reducer, { name: 'todo' });
+        expect(typeof hor).toBe('function');
+    });
+
+    it('should forward the state and the action to the passed reducer', () => {
+        const newState = {};
+        const reducer = jest.fn(() => newState);
+        const hor = entityReducer(reducer, { name: 'todo' });
+        const state = {};
+        const action = {};
+        const returnedState = hor(state, action);
+        expect(reducer).toHaveBeenCalledWith(state, action);
+        expect(returnedState).toBe(newState);
+    });
+
+    it('should initialize the state with an empty object', () => {
+        const reducer = state => state;
+        const hor = entityReducer(reducer, { name: 'todo' });
+        const initialState = hor(undefined, {});
+        expect(initialState).toEqual({});
+    });
+
+    describe('configuration', () => {
+        it('should throw if name is not a string', () => {
+            expect(() => {
+                entityReducer(() => {}, { name: 1 });
+            }).toThrow(new Error(
+                'The reducer higher order reducer should be passed a string for name',
+            ));
+        });
+    });
+
+    describe('normalization', () => {
+        const reducer = state => state;
+        const actionTypes = [
+            'GET_ONE_TODO',
+            'GET_TODO_LIST',
+        ];
+        const hor = entityReducer(reducer, {
+            name: 'todo',
+            actionTypes,
+        });
+        const todo1 = { id: 1, content: 'do not forget' };
+        const getOneTodo = {
+            type: 'GET_ONE_TODO',
+            payload: normalize(todo1, todoSchema),
+        };
+        const stateAfterOneTodo = hor(undefined, getOneTodo);
+
+        it('should normalize the entities found in the allowed actions', () => {
+            expect(stateAfterOneTodo[1]).toEqual(todo1);
+        });
+
+        const todo2 = { id: 2, content: 'hmmm' };
+        const todo3 = { id: 3, content: 'nope' };
+        const getTodoList = {
+            type: 'GET_TODO_LIST',
+            payload: normalize([todo2, todo3], [todoSchema]),
+        };
+
+        const stateAfterTodoList = hor(stateAfterOneTodo, getTodoList);
+        it('should normalize subsequent actions', () => {
+            expect(stateAfterTodoList[2]).toEqual(todo2);
+            expect(stateAfterTodoList[3]).toEqual(todo3);
+        });
+
+        it('should change previsouly loaded entities', () => {
+            expect(stateAfterTodoList[1]).toEqual(todo1);
+        });
+
+        it('should not mutate the state', () => {
+            hor(stateAfterOneTodo, getTodoList);
+            expect(stateAfterOneTodo[2]).toBe(undefined);
+        });
+    });
+
+    it('should pass the normalized entities through the revive function', () => {
+        const reducer = state => state;
+        const hor = entityReducer(reducer, {
+            name: 'todo',
+            actionTypes: ['GET_ONE_TODO'],
+            revive: todo => ({ ...todo, revived: true }),
+        });
+        const getOneTodo = {
+            type: 'GET_ONE_TODO',
+            payload: normalize({ id: 1, content: 'do not forget' }, todoSchema),
+        };
+        const state = hor(undefined, getOneTodo);
+        expect(state[1].revived).toBe(true);
+    });
+
+    it('should use the optional merger function to solve conflicts', () => {
+        const reducer = state => state;
+        const hor = entityReducer(reducer, {
+            name: 'todo',
+            actionTypes: ['GET_ONE_TODO'],
+            merger: (stateTodo, payloadTodo) => ({ ...stateTodo, value: payloadTodo.value }),
+        });
+        const stateWithInitialTodo = hor(undefined, {
+            type: 'GET_ONE_TODO',
+            payload: normalize({ id: 1, content: 'initial', value: 1 }, todoSchema),
+        });
+        const stateAfterNextTodo = hor(stateWithInitialTodo, {
+            type: 'GET_ONE_TODO',
+            payload: normalize({ id: 1, content: 'new', value: 2 }, todoSchema),
+        });
+        const todo = stateAfterNextTodo[1];
+        expect(todo.content).toBe('initial');
+        expect(todo.value).toBe(2);
+    });
+});
---
+++ src/entityReducer.js
@@ -0,0 +1,40 @@
+const entityReducer = (reducer, options) => {
+    const {
+    actionTypes = [],
+    name,
+    revive = entity => entity,
+    merger = (stateEntity, payloadEntity) => payloadEntity,
+  } = options;
+
+    if (typeof name !== 'string') {
+        throw new Error('The reducer higher order reducer should be passed a string for name');
+    }
+
+    // TODO considers making it a named function for better stack traces
+    return (state = {}, action) => {
+        let updatedState = state;
+
+        // Handle the merge of the received entities and the current state
+        if (actionTypes.includes(action.type)) {
+            const entities = action.payload.entities[name];
+            updatedState = Object.keys(entities).reduce(
+                (mergedState, entityId) => ({
+                    ...mergedState,
+                    // If the entity is already in the state, use the merger function
+                    [entityId]: mergedState[entityId]
+                        ? merger(
+                            mergedState[entityId],
+                            revive(entities[entityId]),
+                        )
+                        : revive(entities[entityId])
+                    ,
+                }),
+                state,
+            );
+        }
+
+        return reducer(updatedState, action);
+    };
+};
+
+export default entityReducer;
