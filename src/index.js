8c73e10687aec7b7022c19be3769142abc5a83d0
Augustin Le Fèvre <aulefevre@kilix.fr>
2017-03-21 19:53:24 +0100

    Add flow typings to entityReducer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// @flow
 export { default as entityReducer } from './entityReducer';
 export { default as createEntitiesReducer } from './createEntitiesReducer';
 export { default as combineReducersWithEntities } from './combineReducersWithEntities';
